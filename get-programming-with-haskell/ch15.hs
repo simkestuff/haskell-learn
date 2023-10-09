a28fcc9d17a01c0386983c0f07790720e8bba572
sinisa <simkestuff@gmail.com>
2023-10-09 11:11:38 +0200

    ch15.hs

---
+++ get-programming-with-haskell/ch15.hs
@@ -0,0 +1,14 @@
+data FourLetterAlphabet = L1 | L2 | L3 | L4 deriving (Enum, Bounded, Show)
+
+rotN :: (Bounded a, Enum a) => Int -> a -> a
+rotN n c = toEnum rotation
+    where halfAlphabet = n `div` 2
+          offset = fromEnum c + halfAlphabet
+          rotation = offset `mod` n
+          
+largestCharNum :: Int
+largestCharNum = fromEnum (maxBound :: Char)
+
+rotChar :: Char -> Char
+rotChar charToEncrypt = rotN sizeOfAlphabet charToEncrypt
+    where sizeOfAlphabet = largestCharNum + 1
